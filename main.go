92927b26ca11125a2ce664f62828bb5213bb8ea7
Muhammad Thoriq <mthoriq0001@gmail.com>
2023-05-03 16:03:18 +0700

    back

--- main.go
+++ main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-	//"fmt"
-	"cobagolang/models"
-	"cobagolang/routes"
+	"fmt"
+	// "cobagolang/models"
+	// "cobagolang/routes"
 )
 
 func main() {
-	// arr := [4]int{1, 2, 3}
-	// arr[2] = 4
-	// fmt.Print(arr)
-	db := models.SetupDB()
-	db.AutoMigrate(&models.Customer{})
+	arr := [4]int{1, 2, 3}
+	arr[2] = 4
+	fmt.Print(arr)
+	// db := models.SetupDB()
+	// db.AutoMigrate(&models.Customer{})
 
-	r := routes.SetupRoutes(db)
-	r.Run()
+	// r := routes.SetupRoutes(db)
+	// r.Run()
 }
